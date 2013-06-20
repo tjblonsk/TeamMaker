85d6099686999bdc75d8ee0abab1838eb175113f
Tim <tim.blonski@gmail.com>
2013-06-19 21:57:45 -0400

    team combinator

---
+++ class_org.rb
@@ -0,0 +1,14 @@
+group = ["Alan Appelstein", "Alice Mottola", "Alina Guzman", "andrew silbersmith", "Anuj Jhunjhunwala", "Ben Israeli", "Beryl Schragger", "Chris Clearfield", "Christine Coulter", "Christopher Huyett", "Christopher Wright", "Corbin Sykes", "Eleyna Whittingham", "Eric Gong", "Jamal Powell", "Jason Darcy", "Kevon Cheung", "Mike Mayer", "Mike Vanger", "Nick Lombardi", "Phillip Lamplugh", "Teddy Cleveland", "Thomas Yu", "Tim Blonski", "Tom Brennan", "Tricia Dougals", "Wesley Carr", "Zachariah Reitano"]
+
+puts "Enter team size:"
+team_size = gets.chomp.to_i
+
+while group.length != 0
+group.shuffle!
+x = group.pop(team_size)
+puts "#{x}"
+end
+
+
+
+
