2f49b4d10fd1ccd271aafb27bc389600dd927dbc
Tim <tim.blonski@gmail.com>
2013-08-19 13:27:58 -0400

    scaffolded firemen and fires

---
+++ advanced_rails/Firehouse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehouse::Application.config.secret_token = 'afc11605db8404e122355ad72bab5b3b5fc88df5fd628b90faf6a0bfe82a5aa666f02dd47eac9b6f793fd1d8e441a9fff0dcb8a5d356a0a83f9d4029ffebda72'
---
+++ advanced_rails/Firehouse/db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  Firefighter.create(name: "Fighter#{n}", age: n + 10 )
+end
+
+9001.times do |n|
+  Fire.create(location: "#{n} main street", intensity: n + 100 )
+end
